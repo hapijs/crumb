2a47b8fdc49b0820b671e4db0ad715b5c843a7d4
Marcus Stong <marcusstong@thebeerstore.local>
2013-11-21 10:45:39 -0500

    example using restful mode

---
+++ example/restful.js
@@ -0,0 +1,40 @@
+var Hapi = require('hapi');
+
+var server = Hapi.createServer('127.0.0.1', 8000);
+
+// Add Crumb plugin
+
+server.pack.require('../', { restful: true}, function(err) {
+     if (err) {
+        console.error('failed loading Crumb plugin');
+     }
+});
+
+server.route([
+
+    // a "crumb" cookie gets set with any request when not using views
+
+    {
+        method: 'GET',
+        path: '/generate',
+        handler: function(request) {
+            // return crumb if desired
+            request.reply('{ "crumb": ' + request.plugins.crumb + ' }');
+        }
+    },
+
+    // request header "X-CSRF-Token" with crumb value must be set in request for this route
+
+    {
+        method: 'PUT',
+        path: '/crumbed',
+        handler: function(request) {
+            request.reply('Crumb route');
+        }
+    },
+
+]);
+
+server.start(function() {
+    console.log('Example restful server running at:', server.info.uri);
+});
