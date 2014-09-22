aba8880701f8e2156b95b4f00a79f4e77b6c410c
Marcus Stong <stongo@gmail.com>
2014-09-22 12:38:32 -0400

    parse wildcards

--- test/index.js
+++ test/index.js
@@ -280,59 +280,7 @@ describe('Crumb', function () {
         });
     });
 
-    it('does not set crumb cookie insecurely', function(done) {
-        var options = {
-            cors: true
-        }
-        var server4 = new Hapi.Server(options);
-        server4.route([
-            {
-                method: 'GET', path: '/1', handler: function (request, reply) {
 
-                    return reply('test');
-                }
-            }
-        ]);
-        server4.pack.register({ plugin: require('../'), options: null }, function (err) {
-            expect(err).to.not.exist;
-            var headers = {};
-            headers['Origin'] = '127.0.0.1';
-            server4.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
-
-                var header = res.headers['set-cookie'];
-                expect(header).to.be.undefined;
-
-                done();
-            });
-        });
-    });
-
-    it('does set crumb cookie if allowOrigins set and CORS enabled', function(done) {
-        var options = {
-            cors: true
-        }
-        var server5 = new Hapi.Server(options);
-        server5.route([
-            {
-                method: 'GET', path: '/1', handler: function (request, reply) {
-
-                    return reply('test');
-                }
-            }
-        ]);
-        server5.pack.register({ plugin: require('../'), options: { allowOrigins: ['127.0.0.1']} }, function (err) {
-            expect(err).to.not.exist;
-            var headers = {};
-            headers['Origin'] = '127.0.0.1';
-            server5.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
-
-                var header = res.headers['set-cookie'];
-                expect(header[0]).to.contain('crumb');
-
-                done();
-            });
-        });
-    });
 
     it('does not validate crumb when "skip" option returns true', function (done) {
         var server6 = new Hapi.Server();
@@ -387,6 +335,171 @@ describe('Crumb', function () {
                 expect(res.statusCode).to.equal(403);
 
                 done();
+            });
+        });
+    });
+
+    it('does not allow "*" for allowOrigins setting', function (done) {
+        var server7 = new Hapi.Server();
+
+        server7.pack.register({ plugin: require('../'), options: { allowOrigins: ['*'] } }, function (err) {
+
+            expect(err).to.exist;
+
+            done();
+        });
+    });
+
+    it('does not set crumb cookie insecurely', function(done) {
+        var options = {
+            cors: true
+        }
+        var server4 = new Hapi.Server(options);
+        server4.route([
+            {
+                method: 'GET', path: '/1', handler: function (request, reply) {
+
+                    return reply('test');
+                }
+            }
+        ]);
+        server4.pack.register({ plugin: require('../'), options: null }, function (err) {
+            expect(err).to.not.exist;
+            var headers = {};
+            headers['Origin'] = '127.0.0.1';
+            server4.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                var header = res.headers['set-cookie'];
+                expect(header).to.be.undefined;
+
+                done();
+            });
+        });
+    });
+
+    it('does set crumb cookie if allowOrigins set and CORS enabled', function(done) {
+        var options = {
+            cors: true
+        }
+        var server5 = new Hapi.Server(options);
+        server5.route([
+            {
+                method: 'GET', path: '/1', handler: function (request, reply) {
+
+                    return reply('test');
+                }
+            }
+        ]);
+        server5.pack.register({ plugin: require('../'), options: { allowOrigins: ['127.0.0.1']} }, function (err) {
+            expect(err).to.not.exist;
+            var headers = {};
+            headers['Origin'] = '127.0.0.1';
+            server5.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                var header = res.headers['set-cookie'];
+                expect(header[0]).to.contain('crumb');
+
+                headers['Origin'] = '127.0.0.2';
+
+                server5.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                    var header = res.headers['set-cookie'];
+                    expect(header).to.be.undefined;
+
+                    headers['Origin'] = '127.0.0.1:2000';
+
+                    server5.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                        var header = res.headers['set-cookie'];
+                        expect(header).to.be.undefined;
+
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    it('checks port for allowOrigins setting', function (done) {
+        var options = {
+            cors: true
+        }
+        var server8 = new Hapi.Server(options);
+        server8.route([
+            {
+                method: 'GET', path: '/1', handler: function (request, reply) {
+
+                    return reply('test');
+                }
+            }
+        ]);
+        server8.pack.register({ plugin: require('../'), options: { allowOrigins: ['127.0.0.1:2000']} }, function (err) {
+            expect(err).to.not.exist;
+            var headers = {};
+            headers['Origin'] = '127.0.0.1:2000';
+            server8.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                var header = res.headers['set-cookie'];
+                expect(header[0]).to.contain('crumb');
+
+                headers['Origin'] = '127.0.0.1:1000';
+                server8.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                    var header = res.headers['set-cookie'];
+                    expect(header).to.be.undefined;
+
+                    headers['Origin'] = '127.0.0.1';
+                    server8.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                        var header = res.headers['set-cookie'];
+                        expect(header).to.be.undefined;
+
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    it('parses wildcards in allowOrigins setting', function (done) {
+        var options = {
+            cors: true
+        }
+        var server9 = new Hapi.Server(options);
+        server9.route([
+            {
+                method: 'GET', path: '/1', handler: function (request, reply) {
+
+                    return reply('test');
+                }
+            }
+        ]);
+        server9.pack.register({ plugin: require('../'), options: { allowOrigins: ['127.0.0.1:*', '*.test.com']} }, function (err) {
+            expect(err).to.not.exist;
+            var headers = {};
+            headers['Origin'] = '127.0.0.1:2000';
+            server9.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                var header = res.headers['set-cookie'];
+                expect(header[0]).to.contain('crumb');
+
+                headers['Origin'] = 'foo.test.com';
+                server9.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                    //expect(header[0]).to.contain('crumb');
+                    expect(header[0]).to.contain('crumb');
+
+                    headers['Origin'] = 'foo.tesc.com';
+
+                    server9.inject({ method: 'GET', url: '/1', headers: headers }, function (res) {
+
+                        var header = res.headers['set-cookie'];
+                        expect(header).to.be.undefined;
+
+                        done();
+                    });
+
+                });
             });
         });
     });
