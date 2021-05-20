5fbd8c16e10c8acc54d9cb7177f02b0b7270de80
Greg <gdelpu@gmail.com>
2021-05-20 07:41:34 +0200

    Fix issue discovered in unit testing

--- test/hmac.js
+++ test/hmac.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
@@ -7,12 +7,12 @@ const Hmac = require('../lib/hmac');
 const { describe, it } = exports.lab = Lab.script();
 const { expect } = Code;
 
-describe('Hmac', ()=>{
+describe('Hmac', () => {
 
     const mySecret = 'super secret!!! Do not share';
     const userId = 'myUserId';
 
-    it('should export two methods', ()=>{
+    it('should export two methods', () => {
 
         expect(Hmac).to.include('encrypt')
             .and
@@ -22,30 +22,45 @@ describe('Hmac', ()=>{
         expect(Hmac.validate).to.be.a.function();
     });
 
-    describe('encrypt', ()=>{
+    describe('encrypt', () => {
 
-        it('should return a digest and a timestamp concatenate with _', ()=>{
+        it('should return a digest and a timestamp concatenate with _', () => {
 
             const result = Hmac.encrypt(userId, mySecret);
-
             expect(result).to.be.a.string().and.to.include('_');
-        })
+        });
     });
 
-    describe('validate', ()=>{
+    describe('validate', () => {
 
-        it('should return TRUE is the token is valid', ()=>{
+        it('should return FALSE if the token is not in the right format', () => {
+
+            let encryptedStr = 'thisIsNotaGoodformat';
+            expect(Hmac.validate(encryptedStr, userId, mySecret)).to.be.a.false();
+
+            encryptedStr = '_thisIsNotaGoodformat';
+            expect(Hmac.validate(encryptedStr, userId, mySecret)).to.be.a.false();
+        });
+
+        it('should return FALSE if one of the parameters is null', () => {
+
+            const encryptedStr = 'thisIsNotaGoodformat';
+
+            expect(Hmac.validate(null, userId, mySecret)).to.be.a.false();
+            expect(Hmac.validate(encryptedStr, null, mySecret)).to.be.a.false();
+            expect(Hmac.validate(encryptedStr, userId, null)).to.be.a.false();
+        });
+
+        it('should return TRUE if the token is valid', () => {
 
             const encryptedStr = Hmac.encrypt(userId, mySecret);
-
             expect(Hmac.validate(encryptedStr, userId, mySecret)).to.be.a.true();
-        })
+        });
 
-        it('should return FALSE if digest do not match', ()=>{
+        it('should return FALSE if digest do not match', () => {
 
             const encryptedStr = `123456ABCD_${Date.now().toString()}`;
-
             expect(Hmac.validate(encryptedStr, userId, mySecret)).to.be.a.false();
-        })
+        });
     });
 });
